18e5ac7a53b2612c4b67ca0cec4c6c40db905ff8
VAGDEVI-2002 <85888838+VAGDEVI-2002@users.noreply.github.com>
2021-07-19 03:53:27 +0530

    Delete 47.digit by conditional oper.c

--- 47.digit by conditional oper.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char character;
-	printf("enter a character: ");
-	scanf("%c",&character);
-	(character>='0' && character<='9')?printf("character is a digit"):printf("character is not a digit");
-}
