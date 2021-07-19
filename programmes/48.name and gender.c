1a05568e54341b089985b0774c5cbf49567fea89
VAGDEVI-2002 <85888838+VAGDEVI-2002@users.noreply.github.com>
2021-07-19 21:38:44 +0530

    Delete 48.name and gender.c

--- programmes/48.name and gender.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char name[20],gender,male;
-	printf("enter name and gender: ");
-	scanf("%c%c",&name,&gender);
-	(gender == male)?printf("mr.%c",name):printf("mrs.%c",name);
-}
