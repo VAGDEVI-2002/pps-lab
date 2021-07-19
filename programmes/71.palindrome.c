7d4bd0fcc9ae1d41bd77f630bddd2cde124e3ff3
VAGDEVI-2002 <85888838+VAGDEVI-2002@users.noreply.github.com>
2021-07-19 22:04:12 +0530

    Add files via upload

---
+++ programmes/71.palindrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int num,rem,number,reverse=0;
+	printf("Enter the number: ");
+	scanf("%d",&num);
+	number = num;
+	while(num>0)
+	{
+		rem = num%10;
+		reverse = reverse*10 + rem ;
+		num/=10;
+	}
+	if(number==reverse)
+		printf("\nThe given number is palindrome.");
+	else
+		printf("\nThe given number is not a palindrome.");
+	
+	return 0;
+}
